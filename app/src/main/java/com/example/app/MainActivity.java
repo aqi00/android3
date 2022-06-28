5fafd32fa00c644f16cd64d3cb76682c043489c7
aqi00 <aqi00@163.com>
2022-06-28 10:30:26 +0800

    第1章  Android开发环境搭建

---
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "我看到你了"); // 添加一行日志信息
+    }
+}
